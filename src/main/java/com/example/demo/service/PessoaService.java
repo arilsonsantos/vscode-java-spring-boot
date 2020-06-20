3e874d6916d5047147b4b8324231de7c8e05e24d
Arilson Santos <arilson@msn.com>
2020-06-20 17:01:40 -0300

    Rest Client

--- src/main/java/com/example/demo/service/PessoaService.java
+++ src/main/java/com/example/demo/service/PessoaService.java
@@ -6,6 +6,7 @@ import com.example.demo.model.Pessoa;
 import com.example.demo.repository.PessoaRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 
@@ -15,8 +16,13 @@ public class PessoaService {
 
     private PessoaRepository pessoaRepository;
 
+    @Transactional(readOnly = true)
     public List<Pessoa> findAll(){
         return pessoaRepository.findAll();
+    }
+
+    public Pessoa save(Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
     }
 
 }
